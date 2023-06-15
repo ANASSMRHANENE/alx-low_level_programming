c9fd3ec05ce1c93afad1254ab283d11a6bd75919
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-15 15:23:54 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
 
-char *message="Programming is like building a multilingual puzzle /n"
+char *message="Programming is like building a multilingual puzzle /n";
 
-puts(message)
+puts(message);
 
-return 0
+return 0;
 }
