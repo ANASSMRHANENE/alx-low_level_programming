f2197e656be4a6dc3c732b3b375eac6fdecc12e7
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-16 15:24:17 -0700

    my commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this programme is about showing
+ * all cambo of digits numbers
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+{
+	putchar(i + '0');
+	if (i != 9)
+	{
+		putchar(', ');
+	}
+}
+putchar('\n');
+return (0);
+}
