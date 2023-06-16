71e8cc2e1bbb2dd974cd821ea2e8f77520efef9a
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-16 11:41:22 -0700

    my commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this project is about number
+ * in hexadicimal
+ *Return: 0(Success)
+ */
+int main(void)
+{
+char hexa;
+for (hexa = '0' ; hexa <= '9' ; hexa++)
+{
+	putchar(hexa);
+}
+for (hexa = 'a' ; hexa <= 'f' ; hexa++)
+{
+	putchar(hexa);
+}
+putchar(hexa);
+}
+return (0);
