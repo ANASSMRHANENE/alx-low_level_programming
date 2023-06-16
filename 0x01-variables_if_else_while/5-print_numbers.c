ecf367e9a4352a5bebfe5b45c7f70247a9e67b31
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-16 08:57:52 -0700

    my commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - shoing numbers from 0 to 10
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int number
+for (number = 0; number <= 10 ; number++)
+{
+	printf("%d", number);
+}
+printf("\n");
+return (0);
+}
