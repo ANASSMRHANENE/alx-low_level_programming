91c74688a03e6ea265da8c7af53c1f403aa69e2c
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-20 13:07:39 -0700

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *main - function print void
+ *Return: 0(SUCCESS)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/** 
+ * main - this void function
+ * shows letters
+ * Return: 0(SUCCESS)
+ */
+void print_alphabet(void)
+{
+char i;
+for(i = 'a' ; i <= 'z' ; i++)
+{
+_putchar(i);	
+}
+_putchar('\n');
+}
