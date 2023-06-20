6cbdc71311111c674ddb8a4ed1e095bbe7e65cc2
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-20 12:34:47 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - this main project is about
+ *_putchar function
+ * Return: 0(SUCCESS)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+return (0);
+}
