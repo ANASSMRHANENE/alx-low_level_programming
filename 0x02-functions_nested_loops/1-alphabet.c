959dc67e0bcbdf7ce6fd978de631b941cd4451de
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-20 13:21:34 -0700

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/** 
+/**
  * main - this void function
  * shows letters
  * Return: 0(SUCCESS)
@@ -7,7 +7,7 @@
 void print_alphabet(void)
 {
 char i;
-for(i = 'a' ; i <= 'z' ; i++)
+for (i = 'a' ; i <= 'z' ; i++)
 {
 _putchar(i);
 }
