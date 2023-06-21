18c1c2eec79f9a735fc6d750db5f9eb39f03fdb3
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-21 11:53:01 -0700

    my commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,56 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - function
  * @n: parametre
- * Return: 0(SUCCESS)
  */
 void print_to_98(int n)
 {
-int i, j, d;
-if (n < 98)
+if (n <= 98)
 {
-for (i = n ; i <= 98 ; i++)
-{
-j = i / 10;
-d = i % 10;
-if (i >= 10)
-{
-_putchar(j + '0');
-_putchar(d + '0');
+	for (; n <= 98 ; n++)
+	{
+		if (n == 98)
+		{
+		printf("%d", n);
+		printf("\n");
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+	}
 }
 else
 {
-_putchar(i + '0');
+	for (; n >= 98 ; n--)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+			printf("\n");
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+	}
 }
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
-else
-{
-for (i = 98 ; i <= n ; i++)
-{
-j = i / 10;
-d = i % 10;
-if (i >= 10)
-{
-_putchar(j + '0');
-_putchar(d + '0');
-}
-else 
-{
-_putchar(i + '0');
-}
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+
+
 }
