0ca245ac4221d636b6f23b9eca81b9404ad94120
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-21 11:28:54 -0700

    my commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,29 +6,49 @@
  */
 void print_to_98(int n)
 {
-int i;
-	if (n < 98)
-	{
-		for (i = n ; i <= 98 ; i++)
-		{
-			_putchar(i + '0');
-			if (i != 98)
-			{
-			_putchar(',');
-			_putchar(' ');
-			}
-		}
-	}
-	else
-	{
-		for (i = 98 ; i <= n ; i++)
-		{
-			_putchar(i + '0');
-			if (i != n)
-			{
-			_putchar(',');
-			_putchar(' ');
-			}
-		}
-	}
+int i, j, d;
+if (n < 98)
+{
+for (i = n ; i <= 98 ; i++)
+{
+j = i / 10;
+d = i % 10;
+if (i >= 10)
+{
+_putchar(j + '0');
+_putchar(d + '0');
+}
+else
+{
+_putchar(i + '0');
+}
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+else
+{
+for (i = 98 ; i <= n ; i++)
+{
+j = i / 10;
+d = i % 10;
+if (i >= 10)
+{
+_putchar(j + '0');
+_putchar(d + '0');
+}
+else 
+{
+_putchar(i + '0');
+}
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
 }
