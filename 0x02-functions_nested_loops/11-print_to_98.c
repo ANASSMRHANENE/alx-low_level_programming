fa76263f71aa47ad784b82a356b517e71736793b
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-21 06:32:32 -0700

    my commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_to_98 - function
+ * @n: parametre
+ * Return: 0(SUCCESS)
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = 98 ; i <= n ; i++)
+		{
+			_putchar(i + '0');
+			if (i != n)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+}
