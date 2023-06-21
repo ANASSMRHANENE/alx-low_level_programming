e450feaa0f6d693f47e55211d47f774b0fd8df70
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-20 19:42:16 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,13 @@ void times_table(void)
 int i, j, r;
 for (i = 0 ; i <= 9 ; i++)
 {
-	for (j = 0 ; j <= 9 ; j++)
+for (j = 0 ; j <= 9 ; j++)
 {
-	r = i * j;
-	_putchar(r + '0');
-	_putchar(' ');
-	_putchar(',');
+r = i * j;
+_putchar(r + '0');
+_putchar(' ');
+_putchar(',');
 }
 _putchar('\n');
+}
 }
