4587899b25bd1ed7d0b73362a4106b8aab1a4802
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-20 19:57:59 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,14 +15,19 @@ if (r > 9)
 {
 _putchar((r / 10) + '0');
 _putchar((r % 10) + '0');
+if (j != 9 || i != 9)
+{
 _putchar(',');
-_putchar(' ');
+}
 _putchar(' ');
 }
 else
 {
 _putchar(r + '0');
+if (j != 9 || i != 9)
+{
 _putchar(',');
+}
 _putchar(' ');
 _putchar(' ');
 }
