1e2085a147aaaea573214dda904887a0e2824bde
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-22 16:22:45 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function of multiplication
+ * @a: parametre
+ * @b: parametre
+ * Return: 0(SUCCESS)
+ */
+int mul(int a, int b)
+{
+	int m;
+	m = a * b;
+	return (m);
+}
