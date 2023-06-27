8cede824a728f5301651e768a14e654d6563f369
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-26 18:02:10 -0700

    my commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,14 @@
  */
 void rev_string(char *s)
 {
-	int i;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	for (i- = 1; i >= 0 ; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+int i;
+while (s[i] != '\0')
+{
+i++;
+}
+for (i - = 1; i >= 0 ; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
