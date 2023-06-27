a142b3d07628e96b7b7a654d0fc4e48e1df28638
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-26 18:11:58 -0700

    my commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,33 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * rev_string - function
- * @s: parametre
- * Return: 0(SUCCESS)
- */
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
 void rev_string(char *s)
 {
-int i;
-char c;
-while (s[i] != '\0')
-{
-i++;
-}
-for (i = i - 1; i >= 0 ; i--)
-{
-c = s[i];
-_putchar(c);
-}
-_putchar('\n');
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
 }
