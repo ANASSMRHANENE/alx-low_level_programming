3b2da38c18a67cd64529550352483398c93b3dc3
ANASSMRHANENE <mrhanene@gmail.com>
2023-06-30 16:54:20 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+include "main.h"
+include <stdio.h>
+/**
+ * main - this function concatenate
+ * @dest: parametre
+ * @src: parametre
+ * @n: parametre
+ * Return: 0(SUCCESS)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j;
+while (dest[i])
+{
+i++;
+}
+for (j = 0 ; j <= n ; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i]='\0';
+return (dest);
+}
