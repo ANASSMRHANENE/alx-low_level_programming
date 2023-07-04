d854b8d0d09ce27810f79213003711ba3668685c
ANASSMRHANENE <mrhanene@gmail.com>
2023-07-04 02:06:42 -0700

    my commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,11 +4,11 @@
  * @s: parametre
  * @b: parametre
  * @n: parametre
- * Return: 0(SUCCESS)
+ * Return: memory filled
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i;
+unsigned int i;
 for (i = 0; i < n; i++)
 {
 s[i] = b;
