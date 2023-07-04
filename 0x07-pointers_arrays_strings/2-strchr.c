b95abc23b24093d9b334920aa17da3db0c768499
ANASSMRHANENE <mrhanene@gmail.com>
2023-07-04 02:30:17 -0700

    my commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,21 @@
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-while (s[i] != '\0')
-{
-if (s[i] == c)
-{
-return (s[i]);
-}
-i++;
-}
-return (null);
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
 }
