af3cb66e47043bae13e91d143b383a70a4241ce0
ANASSMRHANENE <mrhanene@gmail.com>
2023-07-13 16:39:06 -0700

    my commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - function
+ * @min: parametre
+ * @max: parametre
+ * Return: pointer
+ */
+int *array_range(int min, int max)
+{
+int *p;
+int i;
+int j = 0;
+if (min > max)
+{
+return (NULL);
+}
+p = malloc ((max - min + 1) * sizeof(int));
+if (p == NULL)
+{
+return (NULL);
+}
+for (i = min; i <= max; i++)
+{
+p[j] = i;
+j++;
+}
+return (p);
+}
