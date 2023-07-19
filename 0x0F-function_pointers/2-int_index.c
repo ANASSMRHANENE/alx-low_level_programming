63132ed9739b72382fce9db8f8ac77bb347999b8
ANASSMRHANENE <mrhanene@gmail.com>
2023-07-19 16:57:12 -0700

    my commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers"
+#include <stdio.h>
+/**
+ * int_index - function
+ * @array: tab
+ * @size: parametre
+ * @cmp: parametre
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int c;
+
+	if (size <= 0 || cmp == NULL || array == NULL)
+	{
+		return (-1);
+	}
+	for (c = 0; c < size; c++)
+	{
+		if ((*cmp) (array[c]))
+		{
+			return (c);
+		}
+	}
+	return (-1);
+}
