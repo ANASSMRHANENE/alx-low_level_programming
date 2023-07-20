f944bb8dc46816cb9c12196b4a8f50d48668d54b
ANASSMRHANENE <mrhanene@gmail.com>
2023-07-20 14:05:46 -0700

    my commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,13 @@ unsigned int s = 0;
 unsigned int i;
 va_list args;
 va_start(args, n);
-if(n == 0)
+if (n == 0)
 {
 return (0);
 }
 for (i = 0; i < n; i++)
 {
-s += va_arg(args,unsigned int);
+s += va_arg(args, unsigned int);
 }
 va_end(args);
 return (s);
