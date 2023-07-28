c6112ebb819c0bd636d2e44be6af459e9ea925a9
ANASSMRHANENE <mrhanene@gmail.com>
2023-07-27 17:35:21 -0700

    my commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,9 +2,8 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
+ * print_list - print elements of the list
+ * @h: pointer to the list_t list 
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
