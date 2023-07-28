b36b86e82bb3b18246d3619f840c05bcf02dff01
ANASSMRHANENE <mrhanene@gmail.com>
2023-07-27 17:55:02 -0700

    my commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 /**
- * add_node_end - add a new node at the end of list
+ * add_node_end - adds a new node at the end of a linked list
  * @head: double pointer to the list_t list
- * @str: string puted in the new node
+ * @str: string to put in the new node
  *
- * Return: the address of the new element
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
