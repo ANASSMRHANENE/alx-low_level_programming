b07729380ed518b246c71211ba3de23dddd70f49
ANASSMRHANENE <mrhanene@gmail.com>
2023-08-01 17:07:56 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#inlude "lists.h"
+/**
+ * free_listint - function
+ * @head: pointer
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+listint_t *node;
+while (head)
+{
+node = head;
+head = head->next;
+free(node);
+}
+}
