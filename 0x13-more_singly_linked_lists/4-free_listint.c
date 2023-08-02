4457558d768ded1745116c0aa6fb77fd9cb76946
ANASSMRHANENE <mrhanene@gmail.com>
2023-08-01 17:13:48 -0700

    my commit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,8 @@
-#inlude "lists.h"
+#include "lists.h"
+
 /**
- * free_listint - function
- * @head: pointer
- * Return: void
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
