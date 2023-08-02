c801f4007f7d908d53d724c6c6c97a8a0b9aa52b
ANASSMRHANENE <mrhanene@gmail.com>
2023-08-01 18:29:05 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * reverse_listint - function
+ * @head: pointer
+ * Return: pointer
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *before = NULL;
+	listint_t *after = NULL;
+
+	while (*head)
+	{
+		after = (*head)->next;
+		(*head)->next = before;
+		before = *head;
+		*head = after;
+	}
+
+	*head = before;
+
+	return (*head);
+}
