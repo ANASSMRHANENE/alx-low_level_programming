1075df386253ab96ba79f31598a6d5098caf3724
ANASSMRHANENE <mrhanene@gmail.com>
2023-08-03 17:31:47 -0700

    my commit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,16 +8,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, j = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
-
-	for (i = 63; i >= 0; i--)
-	{
-		current = exclusive >> i;
-		if (current & 1)
-			j++;
-	}
-
-	return (j);
+int i, j = 0;
+unsigned long int cur;
+unsigned long int x = n ^ m;
+for (i = 63; i >= 0; i--)
+{
+cur = x >> i;
+if (cur & 1)
+j++;
+}
+return (j);
 }
