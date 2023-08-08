4fe3b1bee4d7674362e4a3e5d66cdc1d89335ec3
ANASSMRHANENE <mrhanene@gmail.com>
2023-08-07 18:40:17 -0700

    my commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file- function
+ * @filename: pointer
+ * @text_content: pointer
+ * Return: If the function fails -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+int r, w, s = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (len = 0; text_content[s];)
+s++;
+}
+r = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(r, text_content, s);
+if (r == -1 || w == -1)
+return (-1);
+close(r);
+return (1);
+}
