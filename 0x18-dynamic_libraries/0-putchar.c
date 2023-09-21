5f03a18acaf0e16b94dc20c5da0e3bc6d8fd5bd5
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-21 15:35:02 -0700

    my commit

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - this main project is about
+ *_putchar function
+ * Return: 0(SUCCESS)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+return (0);
+}
